e2e22f1bd5c7e648692fd3aa0d527118b3d64410
Coop <61427314+codelucid@users.noreply.github.com>
2020-07-30 22:44:48 -0400

    removed navbar to be replaced by header

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 
-import Navbar from "./components/Navbar/navbar";
+
 import './App.css';
 
 
 function App() {
   return (
       <div>
-        <Navbar/>
+        
           {/* <Header />
                     <Project />
                     <Footer /> */}
