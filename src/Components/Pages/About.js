349d1394eb4cb63256843ebf7722faeec9131bd7
Coop <61427314+codelucid@users.noreply.github.com>
2020-07-31 06:57:59 -0400

    updates to About page

--- src/Components/Pages/About.js
+++ src/Components/Pages/About.js
@@ -3,14 +3,14 @@ import React from "react";
 function About() {
   return (
     <div>
-      <h1>About Page</h1>
-      <p>
-        Nunc pharetra finibus est at efficitur. Praesent sed congue diam. Integer gravida dui
-        mauris, ut interdum nunc egestas sed. Aenean sed mollis diam. Nunc aliquet risus ac finibus
-        porta. Nam quis arcu non lectus tincidunt fermentum. Suspendisse aliquet orci porta quam
-        semper imperdiet. Praesent euismod mi justo, faucibus scelerisque risus cursus in. Sed
-        rhoncus mollis diam, sit amet facilisis lectus blandit at.
-      </p>
+      <h1>Cooper Healey</h1>
+      <p>As a native North Carolinian, I enjoy new challenges and helping people in the Raleigh/Durham/Chapel Hill areas.
+      <span class="btn-info">UNC's Coding Boot Camp</span> is teaching me so many new skills--HTML, JavaScript, CSS, 
+      jQuery, Node.js, ES6, mySQL, MongoDB, Mongoose, and React just to name a few!  
+      I am really looking forward to applying my new knowledge, as a Full Stack Developer, 
+      with a local company in the community.</p>
+      <p>Phone: 919-414-2878</p>
+      <p>Email: coophealey@gmail.com</p>
     </div>
   );
 }
