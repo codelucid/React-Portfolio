12f836109542fe26d10eec8f3d0b12b2f55cfd26
Coop <61427314+codelucid@users.noreply.github.com>
2020-07-31 07:04:23 -0400

    creation of blog page and contents

---
+++ src/Components/Pages/Blog.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Blog() {
+  return (
+    <div>
+      <h1>Weekly-Stack Blog</h1>
+      <p>
+       Curious about the boot camp experience?  Please learn about my experience, as well as my fellow classmates' experiences,
+       by exploring our blog!
+      </p>
+      <p><a href="https://weekly-stack.com/" target="_blank"/></p>
+    </div>
+  );
+}
+
+export default Blog;
