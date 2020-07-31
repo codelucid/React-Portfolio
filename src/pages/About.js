4429ed4f5d9debb1da13bdf568740af12094c0a7
Coop <61427314+codelucid@users.noreply.github.com>
2020-07-31 18:59:58 -0400

    including the background image in About page

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,16 +1,19 @@
 import React from "react";
+import Mountain from "../components/Mountain";
 
 function About() {
   return (
     <div>
+      <Mountain backgroundImage="../images/mountain.png">
       <h1 className="text-center">Cooper Healey</h1>
       <p className="text-center">As a native North Carolinian, I enjoy new challenges and helping people in the Raleigh/Durham/Chapel Hill areas.
-      <span class="btn-info">UNC's Coding Boot Camp</span> is teaching me so many new skills--HTML, JavaScript, CSS, 
+      {" "}<span class="btn-info">UNC's Coding Boot Camp</span> is teaching me so many new skills--HTML, JavaScript, CSS, 
       jQuery, Node.js, ES6, mySQL, MongoDB, Mongoose, and React just to name a few!  
       I am really looking forward to applying my new knowledge, as a Full Stack Developer, 
       with a local company in the community.</p>
       <p className="text-center">Phone: 919-414-2878</p>
       <p className="text-center" >Email: coophealey@gmail.com</p>
+      </Mountain>
     </div>
   );
 }
