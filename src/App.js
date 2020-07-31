be03c5bcb4ec76ff82e5b59bcbce1d2fe9e3f1d5
Coop <61427314+codelucid@users.noreply.github.com>
2020-07-31 08:42:03 -0400

    creating project card on app file

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import NavTabs from "./components/NavTabs";
 import Home from "./components/pages/Home";
@@ -6,22 +6,45 @@ import About from "./components/pages/About";
 import Blog from "./components/pages/Blog";
 import Contact from "./components/pages/Contact";
 
+import projects from "./projects.json";
+
 
 {/* <Header />
 <Project />
 <Footer /> */}
-function App() {
-  return (
-    <Router>
-      <div>
-        <NavTabs />
-        <Route exact path="/" component={Home} />
-        <Route exact path="/about" component={About} />
-        <Route exact path="/blog" component={Blog} />
-        <Route path="/contact" component={Contact} />
-      </div>
-    </Router>
-  );
+class App extends Component {
+
+  state = {
+    projects
+  };
+
+  render() {
+    return (
+      <Router>
+        <div>
+          <NavTabs />
+          
+          <Route exact path="/" component={Home} />
+          <Route exact path="/about" component={About} />
+          <Route exact path="/blog" component={Blog} />
+          <Route path="/contact" component={Contact} />
+          {this.state.projects.map(project => (
+            <ProjectCard
+              removeProject={this.removeProject}
+              id={project.id}
+              key={project.id}
+              name={project.name}
+              image={project.image}
+              linkDeployed={project.linkDeployed}
+              linkGithub={project.linkGithub}
+            />
+          ))}
+         
+
+        </div>
+      </Router>
+    );
+  }
 }
 
 export default App;
